ff1aefdce481d25b415235037ca04c888dc5cbae
Jakub Bouček <pan@jakubboucek.cz>
2021-04-21 10:00:08 +0200

    Build script - executable

--- build-mariadb.sh
+++ build-mariadb.sh
@@ -1,9 +1,11 @@
-set -e
+#!/bin/bash
+
+set -eux
 
 ### MariaDB
 docker pull mariadb:10.4
 docker run --rm mariadb:10.4 mysql --version
-docker build --progress plain --no-cache -f mysql/Dockerfile -t jakubboucek/lamp-devstack-mysql:latest mysql/
+docker build --progress plain -f mysql/Dockerfile -t jakubboucek/lamp-devstack-mysql:latest mysql/
 docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10
 docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.4
 docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.4.x
--- build-php.sh
+++ build-php.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # shellcheck disable=SC2086
 set -eux;
 
