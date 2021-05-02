c6bc44952712ac2bfccfc1588a6db68df1edde38
Jakub Bouček <pan@jakubboucek.cz>
2021-05-02 17:24:05 +0200

    MariaDB: Add alpha 10.6

--- build-mariadb.sh
+++ build-mariadb.sh
@@ -2,6 +2,18 @@
 
 set -eux
 
+### MariaDB - alpha 10.6
+docker pull mariadb:10.6
+docker run --rm mariadb:10.6 mysql --version
+docker build --progress plain -f mysql/Dockerfile -t jakubboucek/lamp-devstack-mysql:alpha mysql/
+docker tag jakubboucek/lamp-devstack-mysql:alpha jakubboucek/lamp-devstack-mysql:10-alpha
+docker tag jakubboucek/lamp-devstack-mysql:alpha jakubboucek/lamp-devstack-mysql:10.6-alpha
+docker tag jakubboucek/lamp-devstack-mysql:alpha jakubboucek/lamp-devstack-mysql:10.6.0-alpha
+docker push jakubboucek/lamp-devstack-mysql:10.6.0-alpha
+docker push jakubboucek/lamp-devstack-mysql:10.6-alpha
+docker push jakubboucek/lamp-devstack-mysql:10-alpha
+docker push jakubboucek/lamp-devstack-mysql:alpha
+
 ### MariaDB
 docker pull mariadb:10.5
 docker run --rm mariadb:10.5 mysql --version
