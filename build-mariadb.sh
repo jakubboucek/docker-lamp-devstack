e86e95a164501807e8c4c1c57480800c2279f3c2
Jakub Bouček <pan@jakubboucek.cz>
2021-05-02 17:23:44 +0200

    MariaDB: Upgrade 10.4 -> 10.5

--- build-mariadb.sh
+++ build-mariadb.sh
@@ -3,13 +3,13 @@
 set -eux
 
 ### MariaDB
-docker pull mariadb:10.4
-docker run --rm mariadb:10.4 mysql --version
+docker pull mariadb:10.5
+docker run --rm mariadb:10.5 mysql --version
 docker build --progress plain -f mysql/Dockerfile -t jakubboucek/lamp-devstack-mysql:latest mysql/
 docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10
-docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.4
-docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.4.x
-docker push jakubboucek/lamp-devstack-mysql:latest
+docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5
+docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5.x
+docker push jakubboucek/lamp-devstack-mysql:10.5.x
+docker push jakubboucek/lamp-devstack-mysql:10.5
 docker push jakubboucek/lamp-devstack-mysql:10
-docker push jakubboucek/lamp-devstack-mysql:10.4
-docker push jakubboucek/lamp-devstack-mysql:10.4.x
+docker push jakubboucek/lamp-devstack-mysql:latest
