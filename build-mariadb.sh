8731addac0ae6e2595ef30fa550e2d15cd3b4ac9
Jakub Bouček <pan@jakubboucek.cz>
2021-05-25 12:21:09 +0200

    MariaDB: Add beta 10.6, add text editor `nano`

--- build-mariadb.sh
+++ build-mariadb.sh
@@ -2,22 +2,22 @@
 
 set -eux
 
-### MariaDB - alpha 10.6
-docker pull mariadb:10.6
-docker run --rm mariadb:10.6 mysql --version
-docker build --progress plain -f mysql/Dockerfile -t jakubboucek/lamp-devstack-mysql:alpha mysql/
-docker tag jakubboucek/lamp-devstack-mysql:alpha jakubboucek/lamp-devstack-mysql:10-alpha
-docker tag jakubboucek/lamp-devstack-mysql:alpha jakubboucek/lamp-devstack-mysql:10.6-alpha
-docker tag jakubboucek/lamp-devstack-mysql:alpha jakubboucek/lamp-devstack-mysql:10.6.0-alpha
-docker push jakubboucek/lamp-devstack-mysql:10.6.0-alpha
-docker push jakubboucek/lamp-devstack-mysql:10.6-alpha
-docker push jakubboucek/lamp-devstack-mysql:10-alpha
-docker push jakubboucek/lamp-devstack-mysql:alpha
+### MariaDB - beta 10.6
+docker pull mariadb:beta
+docker run --rm mariadb:beta mysql --version
+docker build --progress plain -f mysql/Dockerfile-10.6 -t jakubboucek/lamp-devstack-mysql:beta mysql/
+docker tag jakubboucek/lamp-devstack-mysql:beta jakubboucek/lamp-devstack-mysql:10-beta
+docker tag jakubboucek/lamp-devstack-mysql:beta jakubboucek/lamp-devstack-mysql:10.6-beta
+docker tag jakubboucek/lamp-devstack-mysql:beta jakubboucek/lamp-devstack-mysql:10.6.1-beta
+docker push jakubboucek/lamp-devstack-mysql:10.6.1-beta
+docker push jakubboucek/lamp-devstack-mysql:10.6-beta
+docker push jakubboucek/lamp-devstack-mysql:10-beta
+docker push jakubboucek/lamp-devstack-mysql:beta
 
 ### MariaDB
 docker pull mariadb:10.5
 docker run --rm mariadb:10.5 mysql --version
-docker build --progress plain -f mysql/Dockerfile -t jakubboucek/lamp-devstack-mysql:latest mysql/
+docker build --progress plain -f mysql/Dockerfile-10.5 -t jakubboucek/lamp-devstack-mysql:latest mysql/
 docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10
 docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5
 docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5.x
