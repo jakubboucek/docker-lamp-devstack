03b077b3bae7132a3d25ab19bc62686526affa0b
Jakub Bouček <pan@jakubboucek.cz>
2021-07-14 09:45:09 +0200

    Build: Reorder PHP build items to better profile at Docker Hub

--- build-php.sh
+++ build-php.sh
@@ -3,11 +3,12 @@
 # shellcheck disable=SC2086
 set -eux;
 
+./php/build-php-8.1-rc-cli.sh
+./php/build-php-8.1-rc.sh
+
 ./php/build-php-7.3-cli.sh
 ./php/build-php-7.3.sh
 ./php/build-php-7.4-cli.sh
 ./php/build-php-7.4.sh
 ./php/build-php-8.0-cli.sh
 ./php/build-php-8.0.sh
-./php/build-php-8.1-rc.sh
-./php/build-php-8.1-rc-cli.sh
