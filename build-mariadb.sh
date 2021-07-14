80e33250e7a96b068d3213a6f4771071982ddaae
Jakub Bouček <pan@jakubboucek.cz>
2021-07-14 09:18:12 +0200

    MySQL: 10.6 RC is now stable

--- build-mariadb.sh
+++ build-mariadb.sh
@@ -2,26 +2,28 @@
 
 set -eux
 
-### MariaDB - beta 10.6
-docker pull mariadb:rc
-docker run --rm mariadb:rc mysql --version
-docker build --progress plain -f mysql/Dockerfile-10.6 -t jakubboucek/lamp-devstack-mysql:rc mysql/
-docker tag jakubboucek/lamp-devstack-mysql:rc jakubboucek/lamp-devstack-mysql:10-rc
-docker tag jakubboucek/lamp-devstack-mysql:rc jakubboucek/lamp-devstack-mysql:10.6-rc
-docker tag jakubboucek/lamp-devstack-mysql:rc jakubboucek/lamp-devstack-mysql:10.6.2-rc
-docker push jakubboucek/lamp-devstack-mysql:10.6.2-rc
-docker push jakubboucek/lamp-devstack-mysql:10.6-rc
-docker push jakubboucek/lamp-devstack-mysql:10-rc
-docker push jakubboucek/lamp-devstack-mysql:rc
-
-### MariaDB
-docker pull mariadb:10.5
-docker run --rm mariadb:10.5 mysql --version
-docker build --progress plain -f mysql/Dockerfile-10.5 -t jakubboucek/lamp-devstack-mysql:latest mysql/
-docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10
-docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5
-docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5.x
-docker push jakubboucek/lamp-devstack-mysql:10.5.x
-docker push jakubboucek/lamp-devstack-mysql:10.5
+### MariaDB - 10.6
+MARIADB_RELEASE=3
+docker pull mariadb:10.6.${MARIADB_RELEASE}
+docker tag mariadb:10.6.${MARIADB_RELEASE} mariadb:10.6
+docker run --rm mariadb:10.6 mysql --version
+docker build --progress plain -f mysql/Dockerfile-10.6 -t jakubboucek/lamp-devstack-mysql:10.6 mysql/
+docker tag jakubboucek/lamp-devstack-mysql:10.6 jakubboucek/lamp-devstack-mysql:latest
+docker tag jakubboucek/lamp-devstack-mysql:10.6 jakubboucek/lamp-devstack-mysql:10
+docker tag jakubboucek/lamp-devstack-mysql:10.6 jakubboucek/lamp-devstack-mysql:10.6
+docker tag jakubboucek/lamp-devstack-mysql:10.6 jakubboucek/lamp-devstack-mysql:10.6.${MARIADB_RELEASE}
+docker push jakubboucek/lamp-devstack-mysql:10.6.${MARIADB_RELEASE}
+docker push jakubboucek/lamp-devstack-mysql:10.6
 docker push jakubboucek/lamp-devstack-mysql:10
 docker push jakubboucek/lamp-devstack-mysql:latest
+
+### MariaDB - 10.5
+MARIADB_RELEASE=11
+docker pull mariadb:10.5.${MARIADB_RELEASE}
+docker tag mariadb:10.5.${MARIADB_RELEASE} mariadb:10.5
+docker run --rm mariadb:10.5 mysql --version
+docker build --progress plain -f mysql/Dockerfile-10.5 -t jakubboucek/lamp-devstack-mysql:10.5 mysql/
+docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5
+docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5.${MARIADB_RELEASE}
+docker push jakubboucek/lamp-devstack-mysql:10.5.${MARIADB_RELEASE}
+docker push jakubboucek/lamp-devstack-mysql:10.5
