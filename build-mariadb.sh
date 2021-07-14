28ae3e76daedf48bdc81fb7babdbb8f758d87fac
Jakub Bouček <pan@jakubboucek.cz>
2021-07-14 09:19:08 +0200

    Build: Reorder MariaDB build items to better profile at Docker Hub

--- build-mariadb.sh
+++ build-mariadb.sh
@@ -2,6 +2,17 @@
 
 set -eux
 
+### MariaDB - 10.5
+MARIADB_RELEASE=11
+docker pull mariadb:10.5.${MARIADB_RELEASE}
+docker tag mariadb:10.5.${MARIADB_RELEASE} mariadb:10.5
+docker run --rm mariadb:10.5 mysql --version
+docker build --progress plain -f mysql/Dockerfile-10.5 -t jakubboucek/lamp-devstack-mysql:10.5 mysql/
+docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5
+docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5.${MARIADB_RELEASE}
+docker push jakubboucek/lamp-devstack-mysql:10.5.${MARIADB_RELEASE}
+docker push jakubboucek/lamp-devstack-mysql:10.5
+
 ### MariaDB - 10.6
 MARIADB_RELEASE=3
 docker pull mariadb:10.6.${MARIADB_RELEASE}
@@ -16,14 +27,3 @@ docker push jakubboucek/lamp-devstack-mysql:10.6.${MARIADB_RELEASE}
 docker push jakubboucek/lamp-devstack-mysql:10.6
 docker push jakubboucek/lamp-devstack-mysql:10
 docker push jakubboucek/lamp-devstack-mysql:latest
-
-### MariaDB - 10.5
-MARIADB_RELEASE=11
-docker pull mariadb:10.5.${MARIADB_RELEASE}
-docker tag mariadb:10.5.${MARIADB_RELEASE} mariadb:10.5
-docker run --rm mariadb:10.5 mysql --version
-docker build --progress plain -f mysql/Dockerfile-10.5 -t jakubboucek/lamp-devstack-mysql:10.5 mysql/
-docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5
-docker tag jakubboucek/lamp-devstack-mysql:latest jakubboucek/lamp-devstack-mysql:10.5.${MARIADB_RELEASE}
-docker push jakubboucek/lamp-devstack-mysql:10.5.${MARIADB_RELEASE}
-docker push jakubboucek/lamp-devstack-mysql:10.5
