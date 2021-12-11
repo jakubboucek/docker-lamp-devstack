1e5e04d3038a5fc4c737eb03d4a51739e7d4b3e5
Jakub Bouček <pan@jakubboucek.cz>
2021-12-11 19:19:45 +0100

    Add check script

---
+++ check-pulls.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# shellcheck disable=SC2086
+set -eux;
+
+docker pull php:7.3-cli-bullseye
+docker pull php:7.3-apache-bullseye
+docker pull php:7.4-cli-bullseye
+docker pull php:7.4-apache-bullseye
+docker pull php:8.0-cli-bullseye
+docker pull php:8.0-apache-bullseye
+docker pull php:8.1-cli-bullseye
+docker pull php:8.1-apache-bullseye
+
+docker pull mariadb:10.3
+docker pull mariadb:10.4
+docker pull mariadb:10.5
+docker pull mariadb:10.6
+docker pull mariadb:10.7
