55acc215c0ac3c5dbf65657fcfab6a800345e51d
Jakub Bouček <pan@jakubboucek.cz>
2022-02-12 19:59:21 +0100

    Legacy PHP: Add 5.6 and append sodium-ext to 7.2

---
+++ php/legacy/build-php-5.6.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# shellcheck disable=SC2086
+set -eux;
+
+cd "$(dirname $0)";
+
+### PHP 5.6
+docker pull php:5.6-apache
+docker run --rm php:5.6-apache php --version
+# Origin image uses outdated schema1 manifest format - use DOCKER_BUILDKIT=0
+DOCKER_BUILDKIT=0 docker build --progress plain -f ./Dockerfile-5.6 -t jakubboucek/lamp-devstack-php:5.6-legacy ../
+DOCKER_BUILDKIT=0 docker build --progress plain -f ./Dockerfile-5.6-debug -t jakubboucek/lamp-devstack-php:5.6-legacy-debug ../
+docker run --rm jakubboucek/lamp-devstack-php:5.6-legacy php --version
+docker run --rm jakubboucek/lamp-devstack-php:5.6-legacy-debug php --version
+docker run --rm jakubboucek/lamp-devstack-php:5.6-legacy php -r "echo implode(', ', get_loaded_extensions()) . PHP_EOL;"
+docker run --rm jakubboucek/lamp-devstack-php:5.6-legacy php -r "var_export(gd_info()) . PHP_EOL;"
+docker push jakubboucek/lamp-devstack-php:5.6-legacy-debug
+docker push jakubboucek/lamp-devstack-php:5.6-legacy
