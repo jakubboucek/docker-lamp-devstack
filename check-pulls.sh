4dfcce5b701f2a1d706211307126f1eca0a94fa2
Jakub Bouček <pan@jakubboucek.cz>
2024-05-24 05:53:59 +0200

    Maria DB: Remove 10.3 from pull check script

--- check-pulls.sh
+++ check-pulls.sh
@@ -10,7 +10,6 @@ docker pull php:8.2-apache-bookworm
 docker pull php:8.3-cli-bookworm
 docker pull php:8.3-apache-bookworm
 
-docker pull mariadb:10.3
 docker pull mariadb:10.4
 docker pull mariadb:10.5
 docker pull mariadb:10.6
