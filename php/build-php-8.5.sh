b8c96f23df4bd4b2145e8678b7ce56ed8fbfc0e8
Jakub Bouček <pan@jakubboucek.cz>
2025-11-20 13:34:53 +0100

    Add Xdebug 3.5.0alpha3 for PHP 8.5 RC

--- php/build-php-8.5.sh
+++ php/build-php-8.5.sh
@@ -13,15 +13,20 @@ fi
 
 if [ "${NO_BUILD:-0}" -ne "1" ]; then
     docker build --progress plain -f ./Dockerfile-8.5 -t jakubboucek/lamp-devstack-php:8.5-rc ./
+    docker build --progress plain -f ./Dockerfile-8.5-debug -t jakubboucek/lamp-devstack-php:8.5-rc-debug ./
     PHP_RELEASE=$(docker run --rm jakubboucek/lamp-devstack-php:8.5-rc php -r "echo PHP_RELEASE_VERSION;")
     PHP_RELEASE_EXTRA=$(docker run --rm jakubboucek/lamp-devstack-php:8.5-rc php -r "echo PHP_EXTRA_VERSION;")
     docker tag jakubboucek/lamp-devstack-php:8.5-rc jakubboucek/lamp-devstack-php:8.5.${PHP_RELEASE}-rc
     docker tag jakubboucek/lamp-devstack-php:8.5-rc jakubboucek/lamp-devstack-php:8.5.${PHP_RELEASE}-rc-${PHP_RELEASE_EXTRA}
+    docker tag jakubboucek/lamp-devstack-php:8.5-rc-debug jakubboucek/lamp-devstack-php:8.5.${PHP_RELEASE}-rc-debug
+    docker tag jakubboucek/lamp-devstack-php:8.5-rc-debug jakubboucek/lamp-devstack-php:8.5.${PHP_RELEASE}-rc-debug-${PHP_RELEASE_EXTRA}
 fi
 
 if [ "${NO_TEST:-0}" -ne "1" ]; then
     docker run --rm jakubboucek/lamp-devstack-php:8.5-rc php --version
+    docker run --rm jakubboucek/lamp-devstack-php:8.5-rc-debug php --version
     docker run --rm jakubboucek/lamp-devstack-php:8.5-rc php -r "echo implode(', ', get_loaded_extensions()) . PHP_EOL;"
+    docker run --rm jakubboucek/lamp-devstack-php:8.5-rc-debug php -r "echo implode(', ', get_loaded_extensions()) . PHP_EOL;"
     docker run --rm jakubboucek/lamp-devstack-php:8.5-rc php -r "var_export(gd_info()) . PHP_EOL;"
 fi
 
@@ -31,4 +36,7 @@ if [ "${NO_PUSH:-0}" -ne "1" ]; then
     docker push jakubboucek/lamp-devstack-php:8.5.${PHP_RELEASE}-rc-${PHP_RELEASE_EXTRA}
     docker push jakubboucek/lamp-devstack-php:8.5.${PHP_RELEASE}-rc
     docker push jakubboucek/lamp-devstack-php:8.5-rc
+    docker push jakubboucek/lamp-devstack-php:8.5.${PHP_RELEASE}-rc-debug-${PHP_RELEASE_EXTRA}
+    docker push jakubboucek/lamp-devstack-php:8.5.${PHP_RELEASE}-rc-debug
+    docker push jakubboucek/lamp-devstack-php:8.5-rc-debug
 fi
