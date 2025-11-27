9a5ae879998ac40a7f3732d584ccc8247d1dd500
Jakub Bouček <pan@jakubboucek.cz>
2025-11-27 21:17:14 +0100

    MariaDB: Updates

--- check-pulls.sh
+++ check-pulls.sh
@@ -18,5 +18,5 @@ docker pull mariadb:10.6
 docker pull mariadb:10.11
 docker pull mariadb:11.4
 docker pull mariadb:11.8
-docker pull mariadb:12.0
-docker pull mariadb:12.1-rc
+docker pull mariadb:12.1
+docker pull mariadb:12.2-rc
