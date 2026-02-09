5ac9b5d45344a39336770037e808542399775ffd
Jakub Bouček <pan@jakubboucek.cz>
2026-02-09 18:58:44 +0100

    test: add test if Pie Phar file is not corrupted

---
+++ tools/tests/PharPieTest.php
@@ -0,0 +1,15 @@
+<?php
+/** @noinspection PhpComposerExtensionStubsInspection */
+
+declare(strict_types=1);
+
+use Tester\Assert;
+use Tester\Environment;
+
+require __DIR__ . '/bootstrap.php';
+
+Assert::true(extension_loaded('phar'), 'Extension phar is not loaded.');
+
+$phar = new Phar(__DIR__ . '/../../php/build/pie.phar');
+
+Assert::match('#\d+\.\d+\.\d+#', $phar->getVersion(), 'Version is not in format x.y.z');
